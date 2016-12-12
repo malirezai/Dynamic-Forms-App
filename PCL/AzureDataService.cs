211b611c475b2b05774b11355113e5017e99c3f9
malirezaie <m.alirezaie@gmail.com>
2016-12-12 09:07:22 -0800

    commit with missing JsonSerialize
    commit with missing file

--- PCL/AzureDataService.cs
+++ PCL/AzureDataService.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+
 using Microsoft.WindowsAzure.MobileServices;
 using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
 using Microsoft.WindowsAzure.MobileServices.Sync;
